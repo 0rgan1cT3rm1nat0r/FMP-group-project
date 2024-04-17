a9bbeba84967ad0603a4d825e5402a2209ebeed7
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2024-04-17 11:52:55 +0100

    w
    w

--- Assets/Scripts/dotcontrol.cs
+++ Assets/Scripts/dotcontrol.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class toolcontrol : MonoBehaviour
+public class dotcontrol : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnMouseDown()
-    {
-        paintGM.tooltype = "eraser";
-        //Debug.Log("erase selected");
-
     }
 }
