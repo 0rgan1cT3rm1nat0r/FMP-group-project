c104b5c2ebcad6938f7bd5c742bef23c2c1c262d
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2024-04-24 11:17:17 +0100

    cursor anims
    assets

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTexture, new Vector2(92, 37), CursorMode.Auto);
+    }
+}
